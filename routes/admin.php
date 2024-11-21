f0c050c29ce591e34fc36813988507e4174ff74b
LongDino <longntph45139@fpt.edu.vn>
2024-11-21 17:53:01 +0700

    add routes/admin.php

---
+++ routes/admin.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Admin\SizeController;
+use App\Http\Controllers\Admin\UserController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\SupplierController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+
+    Route::get('/', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+    // viết route admin trong này
+
+});
